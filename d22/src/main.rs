8baf0db0e0a37a86c3c2c71343a06047011bdc7a
Jonas Nikula <jonas.nikula@student.tut.fi>
2020-12-22 14:50:45 +0200

    day22.2

--- d22/src/main.rs
+++ d22/src/main.rs
@@ -1,7 +1,7 @@
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 use std::io::{self, Read, Write};
 
-type Deck = VecDeque<u32>;
+type Deck = VecDeque<usize>;
 
 fn parse_decks(mut lines: std::str::Lines) -> (Deck, Deck) {
     let mut p1 = VecDeque::new();
@@ -30,45 +30,126 @@ fn parse_decks(mut lines: std::str::Lines) -> (Deck, Deck) {
     (p1, p2)
 }
 
-fn play_round(p1: &mut Deck, p2: &mut Deck, round: &mut u32) -> bool {
-    println!("-- Round {:3} --", round);
+#[derive(Eq, Hash, Clone, PartialEq)]
+struct GameState {
+    p1: Deck,
+    p2: Deck,
+}
 
+fn print_state(state: &GameState) {
     print!("Player 1's deck: ");
-    for c in p1.iter() {
+    for c in state.p1.iter() {
         print!("{}, ", c);
     }
     println!("");
 
     print!("Player 2's deck: ");
-    for c in p2.iter() {
+    for c in state.p2.iter() {
         print!("{}, ", c);
     }
     println!("");
-
-    let c1 = p1.pop_front().unwrap();
-    let c2 = p2.pop_front().unwrap();
-
-    println!("Player 1 plays: {}", c1);
-    println!("Player 2 plays: {}", c2);
-
-    if c1 > c2 {
-        p1.push_back(c1);
-        p1.push_back(c2);
-        println!("Player 1 wins the round!");
-    } else {
-        p2.push_back(c2);
-        p2.push_back(c1);
-        println!("Player 2 wins the round!");
-    }
-    *round += 1;
-
-    if p1.is_empty() || p2.is_empty() {
-        return true;
-    }
-    false
 }
 
-fn calc_score(winner: &Deck) -> u32 {
+fn play_round(
+    mut state: GameState,
+    mut round: usize,
+    game: usize,
+    states: &mut HashSet<GameState>,
+) -> (GameState, bool) {
+    loop {
+        println!("-- Round {:3} --", round);
+
+        print_state(&state);
+
+        for prev in states.iter() {
+            if &state == prev {
+                println!("loop: ");
+                return (state.clone(), true);
+            }
+        }
+        states.insert(state.clone());
+
+        let mut p1 = state.p1.clone();
+        let mut p2 = state.p2.clone();
+
+        let c1 = p1.pop_front().unwrap();
+        let c2 = p2.pop_front().unwrap();
+        println!("Player 1 plays: {}", c1);
+        println!("Player 2 plays: {}", c2);
+
+        let next;
+
+        if c1 <= p1.len() && c2 <= p2.len() {
+            let mut p1_copy = VecDeque::new();
+
+            let mut work = p1.clone();
+            for _ in 0..c1 {
+                p1_copy.push_back(work.pop_front().unwrap());
+            }
+
+            let mut p2_copy = VecDeque::new();
+
+            let mut work = p2.clone();
+            for _ in 0..c2 {
+                p2_copy.push_back(work.pop_front().unwrap());
+            }
+
+            let temp = GameState {
+                p1: p1_copy,
+                p2: p2_copy,
+            };
+            let (_, p1_won) = play_game(&temp, game + 1);
+
+            if p1_won {
+                p1.push_back(c1);
+                p1.push_back(c2);
+                println!("Player 1 wins the round!");
+            } else {
+                p2.push_back(c2);
+                p2.push_back(c1);
+                println!("Player 2 wins the round!");
+            }
+
+            next = GameState {
+                p1: p1.clone(),
+                p2: p2.clone(),
+            };
+
+            if p1.is_empty() {
+                return (next, false);
+            }
+            if p2.is_empty() {
+                return (next, true);
+            }
+        } else {
+            if c1 > c2 {
+                p1.push_back(c1);
+                p1.push_back(c2);
+                println!("Player 1 wins the round!");
+            } else {
+                p2.push_back(c2);
+                p2.push_back(c1);
+                println!("Player 2 wins the round!");
+            }
+
+            next = GameState {
+                p1: p1.clone(),
+                p2: p2.clone(),
+            };
+
+            if p1.is_empty() {
+                return (next, false);
+            }
+            if p2.is_empty() {
+                return (next, true);
+            }
+        }
+        round += 1;
+        state = next;
+    }
+}
+
+fn calc_score(winner: &Deck) -> usize {
     let mut mult = 0;
     let mut count = 0;
 
@@ -80,50 +161,57 @@ fn calc_score(winner: &Deck) -> u32 {
     count
 }
 
+fn play_game(state: &GameState, game: usize) -> (GameState, bool) {
+    println!("== Game {:3} ==", game);
+    let mut states = HashSet::new();
+    let ret = play_round(state.clone(), 1, game, &mut states);
+    println!("Game {} over", game);
+    ret
+}
+
 fn main() -> io::Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    let (mut p1, mut p2) = parse_decks(input.lines());
+    let (p1, p2) = parse_decks(input.lines());
+
+    let state = GameState { p1, p2 };
 
     println!("Player 1:");
-    for c in &p1 {
+    for c in &state.p1 {
         println!("{}", c);
     }
     println!("");
 
     println!("Player 2:");
-    for c in &p2 {
+    for c in &state.p2 {
         println!("{}", c);
     }
 
-    let mut round = 1;
-    while !play_round(&mut p1, &mut p2, &mut round) {
-        println!("");
-    }
+    let (end_state, p1_won) = play_game(&state, 1);
 
     println!("");
     println!("");
     println!("== Post-game results ==");
 
     print!("Player 1's deck: ");
-    for c in p1.iter() {
+    for c in end_state.p1.iter() {
         print!("{}, ", c);
     }
     println!("");
 
     print!("Player 2's deck: ");
-    for c in p2.iter() {
+    for c in end_state.p2.iter() {
         print!("{}, ", c);
     }
     println!("");
 
+    let winner = match p1_won {
+        true => end_state.p1,
+        false => end_state.p2,
+    };
     let score;
-    if p1.is_empty() {
-        score = calc_score(&p2);
-    } else {
-        score = calc_score(&p1);
-    }
+    score = calc_score(&winner);
 
     writeln!(io::stdout(), "Score: {}", score)?;
 
